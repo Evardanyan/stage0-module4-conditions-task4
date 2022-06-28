73d90cd99dc6ef95cd2bab44d5cd5cadd2c55661
Edgar Vardanyan <e.vardan4an@gmail.com>
2022-06-29 00:31:16 +0400

    first_try

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        if (first > second && second <= third) {
+            System.out.println(first);
+        } else if (second > first && first <= third) {
+            System.out.println(second);
+        } else if (third > first && first <= second) {
+            System.out.println(third);
+        } else {
+            System.out.println(first);
+        }
     }
 }
